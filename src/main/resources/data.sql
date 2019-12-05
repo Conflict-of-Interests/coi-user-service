7ce392ce544d84a21a7448483f1e91c82304a673
AdamDKing <adamdentonking@gmail.com>
2019-12-05 12:45:12 -0500

    enable h2 local and add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO USER_T VALUES
+	(1, 'BATCH-0001', 'Max', 'Planck', 'mplanck'),
+	(2, 'BATCH-0001', 'Niels', 'Bohr', 'nbohr'),
+	(3, null, 'Marie', 'Curie', 'mcurie'),
+	(4, 'BATCH-0002', 'Margaret', 'Mead', 'mmead'),
+	(5, null, 'Claude', 'Levi-Strauss', 'clevistrauss');
