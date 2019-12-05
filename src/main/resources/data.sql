23c7046d8fa39b2645f16aca4b3353a6cd7724cb
AdamDKing <adamdentonking@gmail.com>
2019-12-05 13:04:58 -0500

    fixed id generation for data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO USER_T VALUES
-	(1, 'BATCH-0001', 'Max', 'Planck', 'mplanck'),
-	(2, 'BATCH-0001', 'Niels', 'Bohr', 'nbohr'),
-	(3, null, 'Marie', 'Curie', 'mcurie'),
-	(4, 'BATCH-0002', 'Margaret', 'Mead', 'mmead'),
-	(5, null, 'Claude', 'Levi-Strauss', 'clevistrauss');
+INSERT INTO USER_T (BATCH_CODE, FIRST_NAME, LAST_NAME, USERNAME) VALUES
+	('BATCH-0001', 'Max', 'Planck', 'mplanck'),
+	('BATCH-0001', 'Niels', 'Bohr', 'nbohr'),
+	(null, 'Marie', 'Curie', 'mcurie'),
+	('BATCH-0002', 'Margaret', 'Mead', 'mmead'),
+	(null, 'Claude', 'Levi-Strauss', 'clevistrauss');
